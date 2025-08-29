eed219d4d1aafcfa910d65c694a8c58bfef7a258
Bansari Shah <bansari25.shah@gmail.com>
2025-08-29 19:29:04 +0200

    Initial commit: Spring Boot project structure

---
+++ src/main/java/com/bansarishah/QuoteVerse/QuoteVerseApplication.java
@@ -0,0 +1,13 @@
+package com.bansarishah.QuoteVerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteVerseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bansarishah/QuoteVerse/QuoteVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bansarishah.QuoteVerse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteVerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
